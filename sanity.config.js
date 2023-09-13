35e611f58a800e455e075c226f54680bc2ad36d7
ritesh-chaudhari-aepl <techteam@urdoer.com>
2023-09-13 13:38:54 +0530

    initials

--- sanity.config.js
+++ sanity.config.js
@@ -1,7 +1,9 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
+import {dashboardTool} from '@sanity/dashboard'
 import {schemaTypes} from './schemas'
+import {netlifyWidget} from 'sanity-plugin-dashboard-widget-netlify'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +12,32 @@ export default defineConfig({
   projectId: 'l5o6s57n',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool(),
+    visionTool(),
+    dashboardTool({
+      widgets: [
+        netlifyWidget({
+          title: 'My Netlify deploys',
+          sites: [
+            {
+              title: 'Sanity Studio',
+              apiId: 'xxxxx-yyyy-zzzz-xxxx-yyyyyyyy',
+              buildHookId: 'xxxyyyxxxyyyyxxxyyy',
+              name: 'sanity-gatsby-blog-20-studio',
+            },
+            {
+              title: 'Website',
+              apiId: 'yyyyy-xxxxx-zzzz-xxxx-yyyyyyyy',
+              buildHookId: 'yyyyxxxxxyyyxxdxxx',
+              name: 'sanity-gatsby-blog-20-web',
+              url: 'https://my-sanity-deployment.com',
+            },
+          ],
+        }),
+      ],
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
